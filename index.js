53682af1d86130485e3534767e6386d1a0204a2b
Jahid Hasan <jahidhasan2856@gmail.com>
2022-10-19 09:15:35 +0600

    vercel config added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 const errorHandler = require("./middleware/errorHandler");
 const userRouter = require("./routes/v1/users.route");
 const { body } = require("express-validator");
